214e6066834f97b9f6a100e479d1356a9674c7df
csmahmud99 <csmahmud.2021@gmail.com>
2023-09-22 04:45:09 +0600

    Code correction in Problem-5

--- arrayPosition.js
+++ arrayPosition.js
@@ -8,23 +8,22 @@ b) Remove 'Orange' and add 'Watermelon'.
 
 var fruits = ["Apple", "Banana", "Orange"];
 
-var positionOfBanana = fruits.indexOf("Banana");
-
 // Finding the index (position) of 'Banana'
-console.log("The Index of Banana is:", positionOfBanana);
-// Output (a): The Index of Banana is: 1
+var positionOfBanana = fruits.indexOf("Banana");
+console.log("The Index of 'Banana' is:", positionOfBanana);
+// Output (a) || The Index of 'Banana' is: 1
 
 // Replacing the 'Banana' with 'Mango'
 fruits[1] = "Mango";
-console.log("The new array with Mango is:", fruits);
-// Output (a): The new array with Mango is: [ 'Apple', 'Mango', 'Orange' ]
+console.log("The new array with 'Mango' is:", fruits);
+// Output (a) || The new array with 'Mango' is: [ 'Apple', 'Mango', 'Orange' ]
 
 // Removing the 'Orange' from the last of the array using 'Array pop() Method'
 var removeOrange = fruits.pop();
-console.log("The new array without Orange is:", fruits);
-// Output (b): The new array without Orange is: [ 'Apple', 'Mango' ]
+console.log("The new array without 'Orange' is:", fruits);
+// Output (b) || The new array without 'Orange' is: [ 'Apple', 'Mango' ]
 
 // Adding the 'Watermelon' to the last of the array using 'Array push() Method'
 var addWatermelon = fruits.push("Watermelon");
-console.log("The new array with Watermelon is:", fruits);
-// Output (b): The new array with Watermelon is: [ 'Apple', 'Mango', 'Watermelon' ]
+console.log("The new array with 'Watermelon' is:", fruits);
+// Output (b) || The new array with 'Watermelon' is: [ 'Apple', 'Mango', 'Watermelon' ]
