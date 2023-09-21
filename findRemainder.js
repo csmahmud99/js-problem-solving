4cd07c6c0d230ab7bb8e260de0ba952f0d83eb2f
csmahmud99 <csmahmud.2021@gmail.com>
2023-09-22 03:02:11 +0600

    JS Problem Solving :: Finding Remainder

---
+++ findRemainder.js
@@ -0,0 +1,18 @@
+/* 
+Sarah's mother is teaching her Mathematics. She gave Sarah a number and asked her what the remainder would be if she divided the number by 5. Help Sarah write the program.
+========================================================================================
+Input:
+The first line of the input contains the number.
+
+Output:
+Print the remainder:
+-- Sample Input     : 119
+-- Output           : 4
+*/
+
+var givenNumber = 119;
+var remainder = 119 % 5;
+
+console.log("The remainder is:", remainder);
+
+// Output || The remainder is: 4
