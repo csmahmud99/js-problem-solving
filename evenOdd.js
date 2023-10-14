066b88dca565bc889b4eb296b4c75be3137dae2f
csmahmud99 <csmahmud.2021@gmail.com>
2023-10-14 23:32:10 +0600

    JS Problem Solving :: Finding Even/Odd Number

---
+++ evenOdd.js
@@ -0,0 +1,14 @@
+/* 
+Find a given number is Even or, Odd.
+*/
+
+var num = 100;
+
+if(num % 2 != 0) {
+    console.log(num, ": is an Odd Number");
+}
+else {
+    console.log(num, ": is an Even Number");
+}
+
+// Output || 100 : is an Even Number
